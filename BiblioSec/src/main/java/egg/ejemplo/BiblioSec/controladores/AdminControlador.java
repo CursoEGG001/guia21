bc1964a47938cfc7b6e8a20f8d988c1f7e9c4a92
CursoEGG001 <soygecko@gmail.com>
2024-06-29 20:25:07 -0300

    La cuestion es que no te veo cuando entras...

--- BiblioSec/src/main/java/egg/ejemplo/BiblioSec/controladores/AdminControlador.java
+++ BiblioSec/src/main/java/egg/ejemplo/BiblioSec/controladores/AdminControlador.java
@@ -6,8 +6,10 @@ package egg.ejemplo.BiblioSec.controladores;
 
 import egg.ejemplo.BiblioSec.entidades.Usuario;
 import egg.ejemplo.BiblioSec.servicios.UsuarioServicio;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,30 +24,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/admin")
 public class AdminControlador {
-    
+
     @Autowired
     UsuarioServicio usuarioServicio;
 
     @GetMapping("/dashboard")
-    public String panelAdministrativo() {
+    public String panelAdministrativo(HttpSession httpSession) {
+        Usuario usuario = (Usuario) httpSession.getAttribute("usuariosession");
+        
         return "panel.html";
     }
+
     @GetMapping("/usuarios")
     public String listarUsuarios(ModelMap modelo) {
-        
+
         List<Usuario> usuarios = usuarioServicio.listarUsuarios();
         modelo.addAttribute("usuarios", usuarios);
-        
+
         return "usuario_list.html";
     }
-    
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @GetMapping("/usuario/modificar/{id}")
+    public String modificarUsuarios(@PathVariable String id, ModelMap modelo) {
+
+        Usuario usuario = usuarioServicio.getOne(id);
+        modelo.put("usuario", usuario);
+        return "usuario_modificar.html";
+
+    }
 
     @GetMapping("/modificarRol/{id}")
-    public String cambiarRol(@PathVariable String id){
+    public String cambiarRol(@PathVariable String id) {
         usuarioServicio.cambiarRol(id);
-        
-       return "redirect:/admin/usuarios";
+
+        return "redirect:/admin/usuarios";
     }
-   
-    
+
 }
