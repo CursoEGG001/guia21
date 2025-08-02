c00d7406dd7837a9b25719ad2109e1f6825b176c
CursoEGG001 <soygecko@gmail.com>
2025-08-02 17:09:59 -0300

    Actualización pom y sus consecuentes modificaciones en el us o del framework.

--- live.egg.news2/src/main/java/live/egg/noticia/eggnews/entidades/Periodista.java
+++ live.egg.news2/src/main/java/live/egg/noticia/eggnews/entidades/Periodista.java
@@ -8,23 +8,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import java.io.Serializable;
 import java.util.ArrayList;
-import org.hibernate.annotations.GenericGenerator;
 
 /**
  *
  * @author pc
  */
 @Entity
-@PrimaryKeyJoinColumn(referencedColumnName = "id")
 public class Periodista extends Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private Integer sueldoMensual;
     @OneToMany
